8d8b597592b9cfa38743920852e239de84028cf1
Ganesh Kantle <ganeshkantle@gmail.com>
2025-05-20 15:37:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default defineConfig(async () => {
-  const plugins = [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer()
-          ),
-        ]
-      : []),
-  ];
-
-  return {
-    root: path.resolve(__dirname, "client"),
-    plugins,
-    resolve: {
-      alias: [
-        { find: "@", replacement: path.resolve(__dirname, "client", "src") },
-        {
-          find: "@components",
-          replacement: path.resolve(__dirname, "client", "src", "components"),
-        },
-        {
-          find: "@pages",
-          replacement: path.resolve(__dirname, "client", "src", "pages"),
-        },
-        { find: "@shared", replacement: path.resolve(__dirname, "shared") },
-        {
-          find: "@assets",
-          replacement: path.resolve(__dirname, "attached_assets"),
-        },
-      ],
-    },
-    build: {
-      outDir: path.resolve(__dirname, "dist", "public"),
-      emptyOutDir: true,
-    },
-  };
+export default defineConfig({
+  root: path.resolve(__dirname, "client"),
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "client", "src") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "client", "src", "components"),
+      },
+      {
+        find: "@pages",
+        replacement: path.resolve(__dirname, "client", "src", "pages"),
+      },
+      { find: "@shared", replacement: path.resolve(__dirname, "shared") },
+      {
+        find: "@assets",
+        replacement: path.resolve(__dirname, "attached_assets"),
+      },
+    ],
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+  },
 });
